4d004c52241a3eac6003669b4410fdb0a7bf3127
Sergiusz <deephans@o2.pl>
2018-05-12 15:56:11 +0200

    Added navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {Navbar} from "./navbar/navbar";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Navbar
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
