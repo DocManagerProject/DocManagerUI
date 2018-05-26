af77bb773fcc3dde8f240d71eeead23632a5ae05
Sergiusz <deephans@o2.pl>
2018-05-26 15:48:38 +0200

    added empty loginPage and loginBox components

---
+++ src/modules/login/content/loginpage/loginPage.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'login-page',
+  templateUrl: './loginPage.html',
+  styleUrls: ['./loginPage.css']
+})
+export class LoginPage {
+
+}
--- src/modules/login/login.module.ts
+++ src/modules/login/login.module.ts
@@ -2,6 +2,8 @@ import {RouterModule, Routes} from "@angular/router";
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {LoginComponent} from "./login.component";
+import {LoginPage} from "./content/loginpage/loginPage";
+import {LoginBox} from "./content/loginbox/loginBox";
 
 
 const appRoutes: Routes = [
@@ -11,7 +13,9 @@ const appRoutes: Routes = [
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    LoginPage,
+    LoginBox
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
