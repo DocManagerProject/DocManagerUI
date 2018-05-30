f065ddbb9978a833f638895003fa32a48c8ea2d8
Sergiusz <deephans@o2.pl>
2018-05-30 20:11:27 +0200

    added page service

---
+++ src/modules/documentation/service/pageService.ts
@@ -0,0 +1,24 @@
+import {Injectable} from "@angular/core";
+import {StorageManager} from "../../app/service/storageManager.service";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs/internal/Observable";
+import {environment} from "../../../environments/environment";
+import {Page} from "../model/page";
+
+const API_URL: string = environment.apiUrl;
+
+@Injectable()
+export class PageService {
+  constructor(
+    private http: HttpClient,
+    private storageManager: StorageManager
+  ) { }
+
+  getPage(id: number): Observable<Page> {
+    return this.http.get<Page>(API_URL + "/pages/" + id, {
+      headers: new HttpHeaders({
+        "apiToken": this.storageManager.getApiToken()
+      })
+    });
+  }
+}
