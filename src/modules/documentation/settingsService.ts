c83185a2e00b0a2236f65ec04563e4ca5c05359e
Sergiusz <deephans@o2.pl>
2018-05-30 18:55:21 +0200

    changed rest api url to get user's setting

--- src/modules/documentation/settingsService.ts
+++ src/modules/documentation/settingsService.ts
@@ -12,13 +12,14 @@ export class SettingsService {
 
   constructor(
     private http: HttpClient,
-    private tokenManager: StorageManager
+    private storageManager: StorageManager
   ) { }
 
   getSetting(name: string): Observable<Settings> {
-    return this.http.get<Settings>(API_URL + "/settings/solution/2/name/" + name, {
+    let solutionId: number  = this.storageManager.getSolutionId();
+    return this.http.get<Settings>(API_URL + "/settings/solution/" + solutionId + "/name/" + name, {
       headers: new HttpHeaders({
-        "apiToken": this.tokenManager.getApiToken()
+        "apiToken": this.storageManager.getApiToken()
       })
     });
   }
