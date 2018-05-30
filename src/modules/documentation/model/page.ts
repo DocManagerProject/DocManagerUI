0cc2762ec1d5c73cfadf98695eb203620ccbe34c
Sergiusz <deephans@o2.pl>
2018-05-30 20:08:45 +0200

    added models for PageSection and Page

---
+++ src/modules/documentation/model/page.ts
@@ -0,0 +1,9 @@
+import {Solution} from "./solution";
+
+export class Page {
+  id: number;
+  solution: Solution;
+  name: string;
+  createDate: Date;
+  url: string;
+}
---
+++ src/modules/documentation/model/pageSection.ts
@@ -0,0 +1,7 @@
+export class PageSection {
+  id: number;
+  name: string;
+  content: string;
+  index: number;
+  url: string;
+}
