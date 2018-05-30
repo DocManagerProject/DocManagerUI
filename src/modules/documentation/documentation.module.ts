0a5f381e3eac749e60964d343f07ce155f2ba0eb
Sergiusz <deephans@o2.pl>
2018-05-30 20:03:16 +0200

    renamed Page to PageComponent

--- src/modules/documentation/documentation.module.ts
+++ src/modules/documentation/documentation.module.ts
@@ -3,7 +3,7 @@ import {NgModule} from '@angular/core';
 
 import {DocumentationComponent} from "./documentation.component";
 import {Navbar} from "./navbar/navbar";
-import {Page} from "./content/page/page";
+import {PageComponent} from "./content/page/page.component";
 import {Category} from "./content/category/category";
 import {RouterModule, Routes} from "@angular/router";
 import {Dashboard} from "./content/dashboard/dashboard";
@@ -11,7 +11,7 @@ import {AuthGuard} from "../login/service/authGuard";
 import {SettingsService} from "./service/settingsService";
 
 const documentationRoutes: Routes = [
-  { path: 'page/:id', component: Page, canActivate: [AuthGuard]},
+  { path: 'page/:id', component: PageComponent, canActivate: [AuthGuard]},
   { path: 'category/:id', component: Category, canActivate: [AuthGuard]},
   { path: 'dashboard/:id', component: Dashboard, canActivate: [AuthGuard]}
 ];
@@ -20,7 +20,7 @@ const documentationRoutes: Routes = [
   declarations: [
     DocumentationComponent,
     Navbar,
-    Page,
+    PageComponent,
     Category,
     Dashboard
   ],
